281dbd22750def9053b45049a7f20edb0ffc92d7
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-03-17 08:35:27 +0530

    Best time to buy and sell stock with Transaction fee

---
+++ src/com/govindev/leetcode/challenge/mar21/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,28 @@
+package com.govindev.leetcode.challenge.mar21;
+
+/***
+ * Best Time to Buy and Sell Stock with Transaction Fee
+ * 
+ * https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/590/week-3-march-15th-march-21st/3674/
+ * 
+ * @author Govind
+ *
+ */
+public class BestTimeToBuyAndSellStockWithTransactionFee {
+	public int maxProfit(int[] prices, int fee) {
+		int[] maxProfits = new int[prices.length];
+		maxProfits[prices.length - 1] = 0;
+		for (int i = prices.length - 2; i >= 0; i--) {
+			int j = prices.length - 1;
+			int maxProfit = maxProfits[i + 1];
+			while (i < j) {
+				int profit = ((prices[j] - prices[i]) - fee) + maxProfits[j];
+				maxProfit = (profit > maxProfit) ? profit : maxProfit;
+				j--;
+			}
+			maxProfits[i] = maxProfit;
+		}
+		return maxProfits[0];
+
+	}
+}
